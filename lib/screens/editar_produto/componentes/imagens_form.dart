6afb69d5c8184e0dd00e9a30556d42fe56cdf218
Gilberto Savisky <saviskydeveloper@gmail.com>
2020-06-26 03:25:08 -0300

    editando imagem e chamando camera ou galeria

--- lib/screens/editar_produto/componentes/imagens_form.dart
+++ lib/screens/editar_produto/componentes/imagens_form.dart
@@ -1,6 +1,12 @@
+import 'dart:io';
+
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:fluttericon/fontelico_icons.dart';
+import 'package:fluttericon/typicons_icons.dart';
 import 'package:lojavirtualgigabyte/models/produto.dart';
+import 'package:lojavirtualgigabyte/screens/editar_produto/componentes/image_source_sheet.dart';
 
 class ImagensForm extends StatelessWidget {
 
@@ -18,16 +24,42 @@ class ImagensForm extends StatelessWidget {
           aspectRatio: 1,
           child: Carousel(
             images:
-            state.value.map((imagem){
+            state.value.map<Widget>((imagem){
               return Stack(
                 children: [
+                  if(imagem is String)
+                    Image.network(imagem)
+                  else
+                    Image.file(imagem as File),
                   Align(
                     alignment: Alignment.topRight,
-                    child: Icon(Icons.remove),
+                    child: IconButton(
+                      icon: Icon(FontAwesome5.trash_alt ),
+                      color: Colors.red,
+                      onPressed: (){
+                        state.value.remove(imagem);
+                        state.didChange(state.value);
+                      },
+                    ),
                   ),
                 ],
               );
-            }).toList(),
+            }).toList()..add(
+              Material(
+                color: Colors.grey[100],
+                child: IconButton(
+                  icon: Icon(Icons.add_a_photo),
+                  color: primaryColor,
+                  iconSize: 50,
+                  onPressed: (){
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (_) => ImageSourceSheet(),
+                    );
+                  },
+                ),
+              ),
+            ),
             dotSize: 4,
             dotBgColor: Colors.transparent,
             dotColor: primaryColor,
